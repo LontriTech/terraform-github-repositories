32c25c2194217d16eae0a7aef4402e14d9864c6b
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2024-09-22 20:22:57 -0300

    BUMP Terraform Version to ~> 1.9 && GitHub provider to ~> 6.3

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 /**
  * # Terraform GitHub: Repositories
  *
- * This is a GitHub module for deploying repositories.
+ * This is a Terraform module for deploying GitHub repositories.
  *
  * [![Terraform Docs](https://github.com/lontritech/terraform-github-repositories/actions/workflows/terraform-docs.yml/badge.svg)](https://github.com/lontritech/terraform-github-repositories/actions/workflows/terraform-docs.yml)
  * [![Checkov](https://github.com/lontritech/terraform-github-repositories/actions/workflows/checkov.yml/badge.svg)](https://github.com/lontritech/terraform-github-repositories/actions/workflows/checkov.yml)
  */
 
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.9.0"
 
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
+      source = "integrations/github"
+      version = "~> 6.3.0"
     }
   }
 }
